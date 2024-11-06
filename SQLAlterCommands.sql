7ff1d8caeb2469cffe2b88c243f9be2c3fc37dc3
Hrushikesh Zarekar <rushikeshzarekar265@gmail.com>
2024-11-06 20:14:49 +0530

    added alter table commands

---
+++ SQLAlterCommands.sql
@@ -0,0 +1,12 @@
+Select * From employee_info;
+
+Alter Table employee_info
+Add JoiningDate Date;
+
+Insert Into employee_info (JoiningDate) Values
+('2024-01-15'),
+('2024-02-20'),
+('2024-03-10'),
+('2024-04-05'),
+('2024-04-05');
+
