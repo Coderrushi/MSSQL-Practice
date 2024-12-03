d37f28a1db9a8608b4e88aa4bb64501916802a0c
Hrushikesh Zarekar <rushikeshzarekar265@gmail.com>
2024-12-03 11:54:38 +0530

    add SQL String Functions file

---
+++ SQLStringFunctions.sql
@@ -0,0 +1,8 @@
+Select empName, LEN(empName) from employee_info;
+Select LOWER('THIS IS A STATEMENT');
+Select empName, UPPER(empName) From employee_info;
+Select LTRIM('    anything');
+Select RTRIM('    anything  '); 
+Select SUBSTRING('Microsoft',1,5);
+Select REPLACE('Microsoft', 'soft', 'hard');
+Select REPLICATE('Dummy ', 5);
